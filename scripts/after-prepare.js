0128a18766e274fc40700092c61f993a7b2b9e94
Sam Gabriel <sgabriel@brainfuse.com>
2018-05-18 19:38:31 -0400

    Updated to use replaceAll

--- scripts/after-prepare.js
+++ scripts/after-prepare.js
@@ -4,7 +4,7 @@ var parser = require('xml2js');
 
 const PLUGIN_NAME         = "cordova-android-support-gradle-release";
 const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'build.gradle');
-const PACKAGE_PATTERN = /(compile "com.android.support:support-v4:)([^"]+)"/;
+const PACKAGE_PATTERN = /(compile "com.android.support:[^:]+:)([^"]+)"/;
 
 // 1. Parse cordova.xml file and fetch this plugin's <variable name="ANDROID_SUPPORT_VERSION" />
 fs.readFile(path.resolve(process.cwd(), 'config.xml'), function (err, data) {
@@ -42,7 +42,7 @@ function setGradleVersion(version) {
             return console.log(PLUGIN_NAME, " ERROR: ", err);
         }
         contents = contents.toString();
-        fs.writeFile(GRADLE_FILENAME, contents.replace(PACKAGE_PATTERN, "$1" + version + '"'), 'utf8', function (err) {
+        fs.writeFile(GRADLE_FILENAME, contents.replaceAll(PACKAGE_PATTERN, "$1" + version + '"'), 'utf8', function (err) {
             if (err) return console.log(PLUGIN_NAME, ": FAILED TO WRITE ", GRADLE_FILENAME, " > ", version, err);
             console.log(PLUGIN_NAME, ": WROTE ", GRADLE_FILENAME, " > ", version);
         });
